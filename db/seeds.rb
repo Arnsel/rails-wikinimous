9761f1fac9a2f14987c68c796498334dd681b166
Andreas Arnsel <arnsel@pm.me>
2022-11-22 15:35:32 +0100

    everything is working, just need front-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,7 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  Article.create(title: "lorem ipsum", content: "content content")
+end
